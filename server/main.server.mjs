d841a47a1f8dbc5151cdb9c6fae2b39808cd4f19
Irfan Tamboli <ketan.sutar@amelioratesolutions.com>
2025-02-19 11:45:38 +0530

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-V66X2HAO.mjs";import"./chunk-6IRRMIUY.mjs";import"./chunk-FAURT4EH.mjs";import"./chunk-5LBQ7C54.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
